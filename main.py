841d4d54f317b937d93424d656e7b1936b291f25
Sagar Chamling <sgr.raee@gmail.com>
2019-02-06 01:05:38 +0545

    Update API with decorator, logging config

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-import logging
+import config
+import constants
+import decorators
 
+from http import HTTPStatus
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+logger = config.setup_logger()
 
-logging.basicConfig(level=logging.DEBUG)
+
+@decorators.log(logger, constants.SQUARE)
+def square(number):
+    value = int(number)
+
+    return value ** 2
 
 
 def get_message(name):
@@ -13,16 +22,29 @@ def get_message(name):
 
 
 @app.route("/", methods=["POST"])
-def hello():
+def message():
     data = request.json
 
     try:
-        logging.debug('Data received: {}'.format(data))
+        logger.debug('Data received: {}'.format(data))
         message = get_message(data['name'])
-        logging.debug('Message: {}'.format(message))
+        logger.debug('Message: {}'.format(message))
     except TypeError as err:
-        logging.error('Error: {err}'.format(err=err))
+        logger.error('Error: {}'.format(err))
 
         return jsonify(message="You didn't send name.")
 
     return jsonify(message=message)
+
+
+@app.route("/square", methods=["POST"])
+def hello():
+    data = request.json
+
+    try:
+        number = data['number']
+        result = square(number)
+    except Exception:
+        return jsonify(message="Invalid Payload."), HTTPStatus.BAD_REQUEST
+
+    return jsonify(result=result)
